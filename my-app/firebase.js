3a1a6a200a60144634f0350d275409b0f2760c7c
unknown <Kavish9904>
2025-03-16 16:39:18 +0530

    refactor: Remove sensitive information and use environment variables

--- my-app/firebase.js
+++ my-app/firebase.js
@@ -1,6 +1,17 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore, doc, getDoc, setDoc, onSnapshot, collection, getDocs, query, where, orderBy } from "firebase/firestore";
+import {
+  getFirestore,
+  doc,
+  getDoc,
+  setDoc,
+  onSnapshot,
+  collection,
+  getDocs,
+  query,
+  where,
+  orderBy,
+} from "firebase/firestore";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -12,12 +23,23 @@ const firebaseConfig = {
   projectId: "my-app-596bd",
   storageBucket: "my-app-596bd.firebasestorage.app",
   messagingSenderId: "637652045525",
-  appId: "1:637652045525:web:f4ce85e4c0b888a17fdc5c"
+  appId: "1:637652045525:web:f4ce85e4c0b888a17fdc5c",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-export { db, doc, getDoc, setDoc, onSnapshot, collection, getDocs, query, where, orderBy };
+export {
+  db,
+  doc,
+  getDoc,
+  setDoc,
+  onSnapshot,
+  collection,
+  getDocs,
+  query,
+  where,
+  orderBy,
+};
 export default app;
